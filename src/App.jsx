8365512d38e7e0748b30de5a146ee3d7e79e5ceb
Sarah <sarisp3260@gmail.com>
2022-08-26 20:56:59 -0500

    firts configurations

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <div className="App bg-primary text-secundary">
+      h1
+      <div className="figure font-beba text-4xl">
+        hellow koala
+      </div>
+      <div className="figure2">
+        loreidnefneif oaiahiduha awdiuauw bcuibwiua
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0D0D0D",
+        secundary: "#F2B705",
+        orange900: "#D95204",
+        orange700: "#F27405",
+        orange400: "#F29F05",
+      },
+
+      fontFamily:{
+        'beba': ['Bebas Neue']
+      }
+    },
+  },
+  plugins: [],
+};
